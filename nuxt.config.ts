dea0baadb99803416ae2bb441b7620cd35c94173
Svetlana Trukhina <sweta4ca@mail.ru>
2026-04-02 16:18:18 +0300

    add deploy

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: { baseURL: '/doc/' },
   compatibilityDate: '2024-11-01',
   future: {
     compatibilityVersion: 4,

b18018a7f919e889af93b5058d534fc0f9e3fe6d
Svetlana <65328736+sosukii@users.noreply.github.com>
2026-06-03 12:14:00 +0000

    fix: switch api base url to VPS

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -138,7 +138,7 @@ export default defineNuxtConfig({
 
   runtimeConfig: {
     public: {
-      apiBase: process.env.NUXT_PUBLIC_API_BASE || 'https://doc-api-r2vu.onrender.com'
+      apiBase: process.env.NUXT_PUBLIC_API_BASE || 'https://api.aventcompany.shop'
     }
   }
 })
